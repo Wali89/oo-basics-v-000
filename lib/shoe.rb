4ca1df0f6bfca362355c49d62c74a7641e588c68
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-24 22:11:38 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
 
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
+
   def initialize(brand)
     @brand = brand
   end
@@ -12,5 +12,5 @@ class Shoe
     puts "Your shoe is good as new!"
     this.condition = 'new'
   end
-  
+
 end
